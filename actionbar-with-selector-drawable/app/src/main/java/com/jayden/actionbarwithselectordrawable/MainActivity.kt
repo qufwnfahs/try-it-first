6e332034ffe9d3cd39814abc375ff2c75baeacdf
qufwnfahs <style_55@naver.com>
2021-02-17 12:13:47 +0900

    Level-list를 활용한 MenuItem icon 변경 - checked 속성은 checkbox 또는 radio button에 지원되는 속성으로 사용하려면 추가적인 작업이 필요 - enabled 속성을 이용하면 Selector가 즉각 반영되지만 disabled 됬을 시 Click Event를 더 이상 받지 못한다는 문제점이 존재 - always로 보여지는 MenuItem의 경우이기에 icon을 바꿔줘야 하여 Level-list로 유지보수가 쉽게 적용해봄

--- actionbar-with-selector-drawable/app/src/main/java/com/jayden/actionbarwithselectordrawable/MainActivity.kt
+++ actionbar-with-selector-drawable/app/src/main/java/com/jayden/actionbarwithselectordrawable/MainActivity.kt
@@ -2,10 +2,54 @@ package com.jayden.actionbarwithselectordrawable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.get
+import com.jayden.actionbarwithselectordrawable.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var optionMenu: Menu? = null
+
+    private var isFavored = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.test_actions, menu)
+        optionMenu = menu
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_favorite -> {
+                isFavored = !isFavored
+                refreshMenuItems()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun refreshMenuItems() {
+        val optionMenu = optionMenu ?: return
+
+        val actionFavorite = optionMenu.findItem(R.id.action_favorite) ?: return
+
+        actionFavorite.icon.level =
+            if (isFavored) ICON_LEVEL_IS_FAVORED else ICON_LEVEL_IS_NOT_FAVORED
+    }
+
+    companion object {
+        const val ICON_LEVEL_IS_FAVORED = 1
+        const val ICON_LEVEL_IS_NOT_FAVORED = 2
     }
 }
